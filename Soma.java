576c90a0c6d76179bc4d3b98638189fe677588d5
Marcos_Davi <104923141+MarcosDaviDev@users.noreply.github.com>
2023-09-15 15:45:11 -0300

    Update Soma.java

--- Soma.java
+++ Soma.java
@@ -10,12 +10,12 @@ public class Soma {
 		int num1, num2 , resultado;
 		
 		//Entrada
-		System.out.println("Digite dois números inteiros, separados por vírgulas para efetuar a soma: ");
+		System.out.println("Digite dois nÃºmeros inteiros, separados por vÃ­rgulas para efetuar a soma: ");
 		String input = sc.nextLine();
 		String[] parts = input.split(",");
 		
 		if (parts.length != 2){
-			System.out.println("Entrada inválida. Digite exatemente dois números inteiros separados por vírgula");
+			System.out.println("Entrada invÃ¡lida. Digite exatemente dois nÃºmeros inteiros separados por vÃ­rgula");
 			sc.close();
 			return;
 		}
@@ -24,15 +24,16 @@ public class Soma {
 			num1 = Integer.parseInt(parts[0].trim());
 			num2 = Integer.parseInt(parts[1].trim());
 		} catch (NumberFormatException e) {
-			 System.out.println("Erro ao converter valores. Certifique-se de que os valores fornecidos são números válidos.");
+			 System.out.println("Erro ao converter valores. Certifique-se de que os valores fornecidos sÃ£o nÃºmeros vÃ¡lidos.");
 	            sc.close();
 	            return;
 		}
 
-		//Cálculo e saída
+		//CÃ¡lculo e saÃ­da
 		resultado = ((num1) + (num2));
-		System.out.println("O resultado da soma é: " + resultado);
+		System.out.println("O resultado da soma Ã©: " + resultado);
 		
+		sc.close();
 		System.exit(0);
 		
 	}
