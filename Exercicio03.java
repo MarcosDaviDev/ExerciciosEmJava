d0d7398a194347c974c0d0e908762498361ad7e5
Marcos_Davi <104923141+MarcosDaviDev@users.noreply.github.com>
2023-09-15 16:29:53 -0300

    Exercício de Java
    Esta calculadora subtrai o resultado da multiplicação de A*B e C*D. A fórmula é (A*B-C*D)

---
+++ Exercicio03.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+		
+		int A, B, C, D, diferenca;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite quatros números (A,B,C,D),separados por vírgula, onde a diferença será calculada pela múltiplicação de A*B será subtraído de C*D.");
+		String input = sc.nextLine();
+		String[] parts = input.split(",");
+		
+		if(parts.length !=4){
+			System.out.println("Entrada inválida. Digite exatemente dois números inteiros separados por vírgula");
+			sc.close();
+			return;
+		}
+		
+		try {
+			A = Integer.parseInt(parts[0].trim());
+			B = Integer.parseInt(parts[1].trim());
+			C = Integer.parseInt(parts[2].trim());
+			D = Integer.parseInt(parts[3].trim());
+		}catch (NumberFormatException e) {
+			 System.out.println("Erro ao converter valores. Certifique-se de que os valores fornecidos são números válidos.");
+	            sc.close();
+	            return;
+		}
+		
+		diferenca = (A*B-C*D);
+		System.out.println("A diferença é: " + diferenca);
+		
+		sc.close();
+	}
+
+}
