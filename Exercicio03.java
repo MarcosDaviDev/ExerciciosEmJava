cd5b62bec77e060b9d5d3a2d8057c55bc19b2a44
Marcos_Davi <104923141+MarcosDaviDev@users.noreply.github.com>
2023-09-22 17:45:25 -0300

    Add files via upload

--- Exercicio03.java
+++ Exercicio03.java
@@ -1,36 +1,27 @@
-import java.util.Scanner;
+package Secao06;
 
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicio03 {
 
 	public static void main(String[] args) {
 		
-		int A, B, C, D, diferenca;
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Digite quatros números (A,B,C,D),separados por vírgula, para realizar o cálculo da formula (A*B-C*D");
-		String input = sc.nextLine();
-		String[] parts = input.split(",");
+		int n = sc.nextInt();
 		
-		if(parts.length !=4){
-			System.out.println("Entrada inválida. Digite os quatros números inteiros separados por vírgula");
-			sc.close();
-			return;
+		for (int i=0; i<n; i++){
+			
+			double a = sc.nextDouble();
+			double b = sc.nextDouble();
+			double c = sc.nextDouble();
+			
+			double media = (a * 2.0 + b * 3.0 + c * 5.0)/10.0;
+			
+			System.out.printf("%.1f%n", media);
 		}
-		
-		try {
-			A = Integer.parseInt(parts[0].trim());
-			B = Integer.parseInt(parts[1].trim());
-			C = Integer.parseInt(parts[2].trim());
-			D = Integer.parseInt(parts[3].trim());
-		}catch (NumberFormatException e) {
-			 System.out.println("Erro ao converter valores. Certifique-se de que os valores fornecidos são números válidos.");
-	            sc.close();
-	            return;
-		}
-		
-		diferenca = (A*B-C*D);
-		System.out.println("A diferença é: " + diferenca);
 		
 		sc.close();
 	}
