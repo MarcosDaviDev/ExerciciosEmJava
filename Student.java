169fb0f460c717dd8b9aae2ec7d5f3f57d575792
Marcos_Davi <104923141+MarcosDaviDev@users.noreply.github.com>
2023-10-16 17:01:56 -0300

    Exercício seção 10
    Classe aluno que identifica quem foi aprovado

--- Student.java
+++ Student.java
@@ -1,20 +1,28 @@
-package entities;
+package Entities;
 
 public class Student {
-	
-	public String name;
-	public double primeiroTrimestre, segundoTrimestre, terceiroTrimestre;
-	
-	public double finalGrade(){
-		return  primeiroTrimestre + segundoTrimestre + terceiroTrimestre;
+
+    private String nome;
+    private double nota1;
+    private double nota2;
+    private double media;
+
+    public Student(String nome, double nota1, double nota2) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+      
+    public double calculaMedia() {
+        return media = (nota1 + nota2) / 2.0;
+    }
+    
+    public double setMedia(){
+    	return media;
+    }
+
+	public String getNome() {
+		return nome;
 	}
-	
-	public double missingPoints(){
-		 if(finalGrade() <= 60){
-			 return 60.0 - finalGrade();
-		 } else {
-			 return 0.0;
-		 }
-	}
-	
+    
 }
