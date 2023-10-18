a9899c2aaf55d7b4050a2df77fa0f56dae676eee
Marcos_Davi <104923141+MarcosDaviDev@users.noreply.github.com>
2023-10-18 17:20:18 -0300

    Exercício Proposto Seção 10
    Classe employee do exercício proposto

--- Employee.java
+++ Employee.java
@@ -1,21 +1,54 @@
 package entities;
 
 public class Employee {
+		
+	private Integer id;
+	private String name;
+	private Double salary;
 	
-	public String name;
-	public double grossSalary;
-	public double tax;
-	
-	public double netSalary(){
-		return grossSalary - tax;
+	public Employee() {
+	}
+
+	public Employee(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
 	}
 	
 	public void increaseSalary(double percentage){
-		   grossSalary += grossSalary * percentage / 100.0;
-		}
+		salary += + salary * percentage / 100.0;
+	}
 	
 	public String toString(){
-		return name + ", US$" + String.format("%.2f", netSalary());
+		return id 
+				+ ", "
+				+ name
+				+ ", "
+				+ String.format("%.2f", salary);
 	}
 	
 }
